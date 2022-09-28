69e3f8c2be498b819212d3baa19facba2e4c7d29
sovazavr <vadimsopov00@mail.ru>
2022-09-28 16:26:43 +0700

    com_one

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
+
+import { useEffect, useLayoutEffect, useRef, useState } from 'react';
 import './App.css';
 
+
+
 function App() {
+  const text = "Проверка ебла блять"
+  const arrText = text.split('')
+  const [symbol, setSymbol] = useState('')
+  const [number, setNumber] = useState(0)
+  const [value, setValue] = useState('')
+  const [end, setEnd] = useState(false)
+  const check = (e) => {
+    setValue(e.target.value)
+
+
+
+
+  }
+  const firstUpdate = useRef(true);
+  useLayoutEffect(() => {
+    if (firstUpdate.current) {
+      firstUpdate.current = false;
+      return;
+    }
+
+    const spanElem = document.getElementById(number)
+    console.log(spanElem);
+    if (spanElem) {
+      if (value == spanElem.innerHTML) {
+        setNumber(number + 1)
+      }
+      setValue('')
+    } else {
+      console.log("елсэ отработал");
+      setEnd(true)
+    }
+
+  }, [value]);
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {end
+        ? (
+          <p>ну молодец иди на хуй</p>
+        )
+        : (
+          <>
+            <input type="text" value={value} autoFocus onChange={check} />
+            {arrText.map((e, i) => (<span key={i} id={i} className={i == number ? "wgreen" : "wblack"}>{e}</span>))}
+          </>
+        )
+      }
+
     </div>
   );
 }
