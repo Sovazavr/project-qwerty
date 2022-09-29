746f9113778075cf7812ca42f2701beb495ba477
Ivan <akimovivan355@gmail.com>
2022-09-29 15:31:40 +0700

    develop branch

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-
-import { useEffect, useLayoutEffect, useRef, useState } from 'react';
-import './App.css';
-
-
+import { useEffect, useLayoutEffect, useRef, useState } from "react";
+import "./App.css";
 
 function App() {
-  const text = "Проверка ебла блять"
-  const arrText = text.split('')
-  const [symbol, setSymbol] = useState('')
-  const [number, setNumber] = useState(0)
-  const [value, setValue] = useState('')
-  const [end, setEnd] = useState(false)
+  const text = "Проверка ебла блять";
+  const arrText = text.split("");
+  const [symbol, setSymbol] = useState("");
+  const [number, setNumber] = useState(0);
+  const [value, setValue] = useState("");
+  const [end, setEnd] = useState(false);
   const check = (e) => {
-    setValue(e.target.value)
-
-
-
-
-  }
+    setValue(e.target.value);
+  };
   const firstUpdate = useRef(true);
   useLayoutEffect(() => {
     if (firstUpdate.current) {
@@ -25,41 +18,39 @@ function App() {
       return;
     }
 
-    const spanElem = document.getElementById(number)
+    const spanElem = document.getElementById(number);
     console.log(spanElem);
     if (spanElem) {
       if (value == spanElem.innerHTML) {
-        setNumber(number + 1)
+        setNumber(number + 1);
       }
-      setValue('')
+      setValue("");
     } else {
       console.log("елсэ отработал");
-      setEnd(true)
+      setEnd(true);
     }
-
   }, [value]);
-
-
 
   return (
     <div className="App">
-      {end
-        ? (
-          <p>ну молодец иди на хуй</p>
-        )
-        : (
-          <div className='textCheck'>
-            <input type="text" value={value} autoFocus onChange={check} />
-            <>
-              {arrText.map((e, i) => (<span key={i} id={i} className={i == number ? "wgreen" : "wblack"}>
+      {end ? (
+        <p>ну молодец иди на хуй</p>
+      ) : (
+        <div className="textCheck">
+          <input type="text" value={value} autoFocus onChange={check} />
+          <>
+            {arrText.map((e, i) => (
+              <span
+                key={i}
+                id={i}
+                className={i == number ? "wgreen" : "wblack"}
+              >
                 {e}
               </span>
-              ))}
-            </>
-          </div>
-        )
-      }
-
+            ))}
+          </>
+        </div>
+      )}
     </div>
   );
 }
