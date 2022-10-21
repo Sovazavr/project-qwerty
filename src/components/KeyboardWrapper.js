5f1a219185aed9d7f4d572e651053629c7681981
Akimov Ivan <akimovivan355@gmail.com>
2022-10-21 13:39:30 +0700

    fix tab, alt

--- src/components/KeyboardWrapper.js
+++ src/components/KeyboardWrapper.js
@@ -1,12 +1,11 @@
-import React, {  useEffect} from 'react'
+import React, {  useCallback, useEffect} from 'react'
 import KeyboardRU from './KeyboardRU'
 
 const KeyboardWrapper = () => {
-    const keyPressing = (e) => {
+    const keyPressing = useCallback((e) => {
+        e.preventDefault()
         const colorRect = "#35587C"
         const colorText = "#9ABADB"
-
-        console.log(e.keyCode);
         const svgElemRect = document.getElementById(e.keyCode).getElementsByTagName('rect')[0]
         const svgElemPaths = document.getElementById(e.keyCode).getElementsByTagName('path')
         svgElemRect.style.fill = colorRect
@@ -14,12 +13,9 @@ const KeyboardWrapper = () => {
         if (svgElemPaths.length > 1) {
             svgElemPaths[1].style.fill = colorText
         }
-
-
-
-    }
-    const keyUnPressing = (e) => {
-
+    })
+    const keyUnPressing = useCallback((e) => {
+        e.preventDefault()
         console.log(e.keyCode);
         const svgElemRect = document.getElementById(e.keyCode).getElementsByTagName('rect')[0]
         const svgElemPaths = document.getElementById(e.keyCode).getElementsByTagName('path')
@@ -28,15 +24,8 @@ const KeyboardWrapper = () => {
         if (svgElemPaths.length > 1) {
             svgElemPaths[1].style.fill = 'black'
         }
-
-    }
-
-
-
-
-
+    })
     useEffect(() => {
-
         document.addEventListener("keydown", keyPressing, false);
         document.addEventListener("keyup", keyUnPressing, false)
         return () => {
